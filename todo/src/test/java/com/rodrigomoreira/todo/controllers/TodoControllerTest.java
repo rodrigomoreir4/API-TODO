108e86c3b5cec5862951a796af7c129dfd319730
Rodrigo Moreira <rodrigomoreir4@gmail.com>
2024-09-20 12:58:49 -0300

    Mudando o BD para MySql

--- todo/src/test/java/com/rodrigomoreira/todo/controllers/TodoControllerTest.java
+++ todo/src/test/java/com/rodrigomoreira/todo/controllers/TodoControllerTest.java
@@ -8,23 +8,19 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.http.MediaType;
 
 import static org.hamcrest.Matchers.hasSize;
-import static org.mockito.Mockito.doNothing;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.mockito.ArgumentMatchers.anyString;
 
 import static com.rodrigomoreira.todo.constants.TodoConstants.TODO;
 
 import java.util.Collections;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
 @WebMvcTest(TodoController.class)
